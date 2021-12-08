1cb783aa251a7ddc7c15d012f04cfff108bc1ba5
Nikita Gritcaenko <nikita-gricaenko@ya.ru>
2021-12-08 23:50:08 +0300

    Fix some coords, update solutions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def run_intelligent_placer(path, polygon):
 
 
 if __name__ == '__main__':
-    polygon_points = [[0, 0], [400, 1200], [1200, 1700], [2100, 1900], [1900, -800]]
+    polygon_points = [[40, 0], [30, 90], [90, 125], [155, 140], [120, -40]]
     for i in range(1, 6):
         path = "PlacerDataset/" + str(i) + ".jpg"
         print(run_intelligent_placer(path, polygon_points))
