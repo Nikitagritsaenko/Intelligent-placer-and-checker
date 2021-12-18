1b6b7c41476de4e7b892e50383c9b8c1be090183
MChepulis <Michael.Chepulis@yandex.ru>
2021-12-18 22:39:57 +0300

    add verbose flag

--- intelligent_placer_lib/intelligent_placer.py
+++ intelligent_placer_lib/intelligent_placer.py
@@ -5,8 +5,10 @@ def check_image(path, polygon=None, verbose=False):
     if polygon is None:
         raise Exception("polygon is empty")
 
-    objects_names, objects_convex_hull = objects_carver.find_objects_on_img(path)
 
-    print(objects_names)
+    objects_names, objects_convex_hull = objects_carver.find_objects_on_img(path, verbose=verbose)
+    if verbose:
+        print(f"detected objects: {objects_names}")
+
     result = placer.can_pack(polygon, objects_names, objects_convex_hull, verbose=verbose)
     return result
